fd58054bdfc7acdd1f436d36439f6a2139eb4ddb
Sarah-0911 <sarah.tabet69@gmail.com>
2025-07-17 16:19:44 +0200

    Affichage conditionnel dans la Navbar ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
+import { sessionInfo } from "@/lib/serverMethods/session/sessionMethods";
 
-export default function Navbar() {
+export default async function Navbar() {
+
+  const session = await sessionInfo();
+  console.log(session)
+
+
   return (
     <nav className="fixed w-full bg-slate-50 border-b border-zinc-300">
       <div className="u-main-container flex py-4 gap-4 text-zinc-900">
         <Link href="/">AXORIA</Link>
         <Link href="/categories" className="mr-auto">Categories</Link>
-        <Link href="/dashboard/create">Add an article</Link>
-        <Link href="/signin">Sign in</Link>
-        <Link href="/signup">Sign up</Link>
+        {session.success ? (
+          <Link href="/dashboard/create">Add an article</Link>
+          // <NavbarDropdown />
+        ) : (
+          <>
+            <Link href="/signin">Sign in</Link>
+            <Link href="/signup">Sign up</Link>
+          </>
+        )
+        }
       </div>
     </nav>
   )
